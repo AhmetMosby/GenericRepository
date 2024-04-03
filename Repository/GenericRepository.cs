b117ca2dbda88957de9fad69382aec3c8794ed8d
BRIGHT <BRIGHT@DESKTOP-2KPKJ12>
2024-04-03 19:51:06 +0300

    Add project files.

---
+++ Repository/GenericRepository.cs
@@ -0,0 +1,31 @@
+﻿
+namespace GenericRepository.Repository
+{
+    public class GenericRepository<TEntity> : IRepository<TEntity>
+    {
+        public void Create(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<TEntity> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Repository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace GenericRepository.Repository
+{
+    public interface IRepository<TEntity>
+    {
+        IQueryable<TEntity> GetAll();
+        IQueryable<TEntity> GetById(int id);   
+
+        void Create(TEntity entity);
+        void Update(int id, TEntity entity);
+        void Delete (int id);
+
+    }
+}
